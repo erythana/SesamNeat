e53e0d833b8d5e7d347694a3ad671f61f8bb5864
Christian Berger <c.berger18@gmail.com>
2024-02-05 11:56:31 +0100

    Further tests

--- SesamNeat.Blazor/obj/Debug/net8.0/SesamNeat.Blazor.AssemblyInfo.cs
+++ SesamNeat.Blazor/obj/Debug/net8.0/SesamNeat.Blazor.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SesamNeat.Blazor")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+092ab94cc8d0831d58354bb4ae4ab217fdc0034d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+562312a048ff867abec8f2d11db05097e35b4921")]
 [assembly: System.Reflection.AssemblyProductAttribute("SesamNeat.Blazor")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SesamNeat.Blazor")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
